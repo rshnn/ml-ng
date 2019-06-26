b208d48cfb5711f0f6213b5a9fe4f669f508e674
Patel <roshan.r.patel2.civ@mail.mil>
2019-06-26 07:39:50 -0400

    progress on anomaly detection

--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -23,6 +23,9 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+    
+    predictions = (pval < epsilon); 
+    
 
 
 
